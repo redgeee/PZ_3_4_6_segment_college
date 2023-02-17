32e7310ff42f9462b6ddceb4f6e65059a2c8eeb8
redgeee <timof.ku@gmail.com>
2023-02-17 09:24:28 +0700

    ПЗ 6, Задание 1

--- PZ_3_and_4/Program.cs
+++ PZ_3_and_4/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            
+            Random rd = new Random();
+
+            // Создание списка отрезков
+            List<LineSegment> lines = new List<LineSegment>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                Point point1 = new Point(rd.Next(-20, 20), rd.Next(-20, 20));
+                Point point2 = new Point(rd.Next(-20, 20), rd.Next(-20, 20));
+                
+                lines.Add(new LineSegment(point1, point2));
+            }
+
+            // Ордината второй точки превышает первую
+            List<LineSegment> newListLines = lines.Where(x => x.Point2.CoordY > x.Point1.CoordY).ToList();
+            newListLines.ForEach(x => Console.WriteLine(x));
+
+            // Сохранение абсцисс точек в отдельный список
+            List<double> ListAbscissaLine = new List<double>();
+
+            ListAbscissaLine.AddRange(newListLines.Select(x => x.Point1.CoordX).ToList());
+            ListAbscissaLine.AddRange(newListLines.Select(x => x.Point2.CoordX).ToList());
         }
     }
 }
